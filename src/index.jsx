0adda0a5b50208887f2a22b8a9e8da60a7533947
thaisondev <thaison123456xyz@gmail.com>
2025-04-13 23:06:05 +0700

    add: add assets

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.jsx';
 import i18n from './translation/i18n.jsx';
 import { I18nextProvider } from 'react-i18next';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import '../public/stylesheets/style.css';
 import '../public/stylesheets/font.css';
 import './index.css';
@@ -21,24 +21,22 @@ import { Provider } from 'react-redux';
 
 import { store, persistor } from './redux/ReduxStore.jsx';
 import { PersistGate } from 'redux-persist/integration/react';
-import LoadingModal from './component/LoadingModal.jsx';
-import ToastContainerComponent from './component/ToastContainerComponent.jsx';
+import LoadingModal from './components/LoadingModal.jsx';
+import ToastContainerComponent from './components/ToastContainerComponent.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-		    <PersistGate loading={null} persistor={persistor}>
-          <Router>
-            <App />
-            <ToastContainerComponent />
-          </Router>
-          <LoadingModal />
-        </PersistGate>
-	    </Provider>
-    </I18nextProvider>
-  </React.StrictMode>,
+  <I18nextProvider i18n={i18n}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+          <ToastContainerComponent />
+        </BrowserRouter>
+        <LoadingModal />
+      </PersistGate>
+    </Provider>
+  </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
